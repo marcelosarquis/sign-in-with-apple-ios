14aee9e49c10dc8cb1ba778b364ccf2a145e345f
Marcelo Sarquis <marcelosarquis@hotmail.com>
2020-04-06 08:13:05 +0200

    Update KeychainItem

--- SignInWithAppleSample/LoginViewController.swift
+++ SignInWithAppleSample/LoginViewController.swift
@@ -94,7 +94,7 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
     
     private func saveUserInKeychain(_ userIdentifier: String) {
         do {
-            try KeychainItem(service: "com.example.apple-samplecode.juice", account: "userIdentifier").saveItem(userIdentifier)
+            try KeychainItem(service: "com.binarieslab.SignInWithAppleSample", account: "userIdentifier").saveItem(userIdentifier)
         } catch {
             print("Unable to save userIdentifier to keychain.")
         }
